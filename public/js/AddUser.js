fd130801a95552adf6587b02608aec914f7f34c1
giang777 <117396536+giang777@users.noreply.github.com>
2023-03-30 04:09:27 +0700

    Update addUser Server

---
+++ public/js/AddUser.js
@@ -0,0 +1,6 @@
+console.log("Add user");
+let name = document.getElementById("fullname");
+let email = document.getElementById("email");
+let pass = document.getElementById("pass");
+let img = document.getElementById("files");
+
